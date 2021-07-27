0364a05abf8006f89169f4063b68e64fe3e83329
franco-123 <francoromangimenez@gmail.com>
2021-07-27 10:58:13 -0300

    agrego vista rol,mesa,previa y colores al general

--- sprint2/app.js
+++ sprint2/app.js
@@ -11,8 +11,8 @@ app.get("/productCart",(req,res) => res.sendFile(path.join(__dirname,"views","pr
 app.get("/register",(req,res) => res.sendFile(path.join(__dirname,"views","register.html")));
 app.get("/login",(req,res) => res.sendFile(path.join(__dirname,"views","login.html")));
 app.get("/productos",(req,res) => res.sendFile(path.join(__dirname,"views","productos.html")));
-app.get("/juegos-de-mesa",(req,res) => res.sendFile(path.join(__dirname,"views","juegos-de-mesa.html")));
-app.get("/juegos-de-rol",(req,res) => res.sendFile(path.join(__dirname,"views","juegos-de-rol.html")));
-app.get("/juegos-de-previa",(req,res) => res.sendFile(path.join(__dirname,"views","juegos-de-previa.html")));
+app.get("/juegos-de-mesa",(req,res) => res.sendFile(path.join(__dirname,"views","juegos-mesa.html")));
+app.get("/juegos-de-rol",(req,res) => res.sendFile(path.join(__dirname,"views","juegos-rol.html")));
+app.get("/juegos-de-previa",(req,res) => res.sendFile(path.join(__dirname,"views","juegos-previa.html")));
 
 app.listen(port,()=> console.log("el puerto esta corriendo en el servidor "+ port))
