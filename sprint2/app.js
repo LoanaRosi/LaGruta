74aa012754fdf8294b9856cd9803b4e5b469f14b
LoanaRosi <loarosi24@gmail.com>
2021-07-27 15:55:15 -0300

    agrego primera instancia de carrito

--- sprint2/app.js
+++ sprint2/app.js
@@ -10,5 +10,12 @@ app.get("/productDetail",(req,res) => res.sendFile(path.join(__dirname,"views","
 app.get("/productCart",(req,res) => res.sendFile(path.join(__dirname,"views","productCart.html")));
 app.get("/register",(req,res) => res.sendFile(path.join(__dirname,"views","register.html")));
 app.get("/login",(req,res) => res.sendFile(path.join(__dirname,"views","login.html")));
+app.get("/formulario1",(req,res) => res.sendFile(path.join(__dirname,"views","formulario1.html")));
+app.get("/formulario2",(req,res) => res.sendFile(path.join(__dirname,"views","formulario2.html")));
+app.get("/formulario3",(req,res) => res.sendFile(path.join(__dirname,"views","formulario3.html")));
+app.get("/formulario4",(req,res) => res.sendFile(path.join(__dirname,"views","formulario4.html")));
+app.get("/formulario5",(req,res) => res.sendFile(path.join(__dirname,"views","formulario5.html")));
+app.get("/formulario6",(req,res) => res.sendFile(path.join(__dirname,"views","formulario6.html")));
+
 
 app.listen(port,()=> console.log("el puerto esta corriendo en el servidor "+ port))
