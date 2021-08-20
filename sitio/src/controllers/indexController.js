77cdb33e128b7864c203da6da8ccb7d24f9c0744
franco-123 <francoromangimenez@gmail.com>
2021-08-19 21:35:25 -0300

    home ejs y carrusel products

--- sitio/src/controllers/indexController.js
+++ sitio/src/controllers/indexController.js
@@ -1,7 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+const products = JSON.parse(fs.readFileSync(path.join(__dirname,"..","data","products.json"),"utf-8"));
 
 module.exports ={
     index : (req,res) =>{
-        return res.render("home")
+        const productsOfert = products.filter(product=>product.sale === true);
+        return res.render("home",{products, productsOfert})
     },
 
     cartShop: (req,res) =>{
