a1e5c1dd6e5efc83806444190c0150b74560fe9f
franco-123 <francoromangimenez@gmail.com>
2021-08-19 23:46:54 -0300

    metodo save, crear, update

--- sitio/src/controllers/indexController.js
+++ sitio/src/controllers/indexController.js
@@ -4,11 +4,13 @@ const path = require("path");
 const products = JSON.parse(fs.readFileSync(path.join(__dirname,"..","data","products.json"),"utf-8"));
 
 module.exports ={
+    // vista del home
     index : (req,res) =>{
         const productsOfert = products.filter(product=>product.sale === true);
         return res.render("home",{products, productsOfert})
     },
 
+    // vita del carrito
     cartShop: (req,res) =>{
         return res.render("productCart")
     }
