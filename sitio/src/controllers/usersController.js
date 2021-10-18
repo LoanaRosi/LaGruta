6d438dfe848df5b0a14b123450f68d2734205ef1
LoanaRosi <loarosi24@gmail.com>
2021-10-18 11:34:45 -0300

    profile

--- sitio/src/controllers/usersController.js
+++ sitio/src/controllers/usersController.js
@@ -10,13 +10,7 @@ module.exports ={
 
     // vista register
     register: (req,res) => { 
-        /* db.Avatar.findAll()
-        .then(avatars => {
-            res.render('user/register', {
-                avatars
-            })
-        })
-        .catch(error => console.log(error)) */
+        
         res.render('user/register')
 
         },
@@ -28,29 +22,19 @@ module.exports ={
                 const {name, email, password,imgUser} = req.body;
                 db.User.create({
                     name : name.trim(),
-                    email : email.trim(),
-                    password : bcrypt.hashSync(password.trim(), 10),
+                    email : name.trim(),
+                    password : bcrypt.hashSync(password, 10),
                     rolId : 2,
-                    avatarId : 1
+                    avatar : req.file ? req.file.filename : "avatar.png",
                 })
-                    .then(user => {
-                        let {imgUser} = req.body
-
-                    db.Avatar.create({
-
-                     
-                        file : imgUser
-                    })
-
+                .then(user => {
                     req.session.userLogin = {
-                    id : user.id,
-                    name : user.name,
-                    rol : user.rol,
-                    img : user.imgUser
-                }
-
-                return res.redirect('/')
-
+                        id : userId,
+                        name : user.name,
+                        avatar : user.avatar,
+                        rolId : user.rolId
+                    }
+                    return res.redirect('/')
                 })
                 .catch(error => console.log(error))
                 
@@ -68,37 +52,68 @@ module.exports ={
     processLogin : (req,res) => {
         let errors = validationResult(req);
 
-        
         if(errors.isEmpty()){
-            let user = users.find(user => user.email === req.body.email.trim());
-
-            req.session.userLogin = {
-                id : user.id,
-                name : user.name,
-                rol : user.rol,
-                img : user.imgUser
-            }
-            const{recordar} = req.body
-            
-            if(recordar){
-                res.cookie("recordame", req.session.userLogin, {maxAge: 365 * 24 * 60 * 60 * 1000})
-            }
-            
-            if(user.rol === "admin"){
-                res.redirect("/user/admin")
-            }
-            res.redirect('profile')
-
+            const {email,recordar} = req.body;
+            db.User.findOne({
+                where : {
+                    email
+                }
+            })
+            .then(user => {
+                req.session.userLogin = {
+                    id : user.id,
+                    name : user.name,
+                    avatar : user.avatar,
+                    rolId : user.rolId
+                }
+                if(recordar){
+                    res.cookie('LaGrutaDelDragon', req.session.userLogin,{maxAge: 365 * 24 * 60 * 60 * 1000})
+                }
+                return res.redirect('/')
+            })
+            .catch(error => console.log(error))
         }else{
             return res.render('user/login',{
                 errors : errors.mapped()
-            })
-        }
+            }
+        )}
     },
 
-    profile : (req,res) => res.render('profile'),
+    profile : (req,res) => {
+        db.User.findByPk(req.session.user.id)
+        .then((user) => {
+            res.send(user)
+            res.render('profileEdit', {
+                user,
+                session: req.session,
+            })
+        })
+        .catch(error => console.log(error))
+    },
 
-    profileEdit: (req, res) => res.render('profileEdit'),
+    profileEdit: (req, res) => {
+        let errors = validationResults(req);
+        
+        if(errors.isEmpty()){
+
+            let {name,email,password} = req.body
+
+            db.User.update({
+                name,
+                email,
+                password,
+                avatar : req.file ? req.file.filename : user.avatar
+            },{
+                where: {
+                    id: req.session.user.id
+                }
+            })
+            .then(() => {
+                res.redirect('profile')
+            })
+            .catch(error => console.log(error))
+        }
+    },
 
     logout : (req,res) => {
         res.cookie("recordame",null,{maxAge : -1})
