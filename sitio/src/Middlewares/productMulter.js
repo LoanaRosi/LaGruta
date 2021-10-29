6afe7654a79a2efbefce56625596a95ffd8a56cc
franco-123 <francoromangimenez@gmail.com>
2021-10-29 11:51:54 -0300

    validaciones back

--- sitio/src/Middlewares/productMulter.js
+++ sitio/src/Middlewares/productMulter.js
@@ -10,10 +10,18 @@ const storage = multer.diskStorage({
     }
 })
 
+const fileFilter = function(req, file,callback) {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif|webp)$/)){
+        req.fileValidationError = "Solo se permite imágenes";
+        return callback(null,false,req.fileValidationError);
+    }
+    callback(null,true);
+}
 
 
 const upload = multer({
     storage,
+    fileFilter
 })
 
 module.exports = upload
