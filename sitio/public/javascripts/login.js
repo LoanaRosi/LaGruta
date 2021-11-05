79031d9fe52034dca36029a1ef4c421ed38afa41
LoanaRosi <loarosi24@gmail.com>
2021-11-05 20:22:33 -0300

    login

--- sitio/public/javascripts/login.js
+++ sitio/public/javascripts/login.js
@@ -1,3 +1,6 @@
+let formLogin = document.querySelector('#formLogin');
+let formError = document.getElementById("form-error");
+
 function qs(element) {
     return document.querySelector(element);
 }
@@ -7,8 +10,7 @@ window.addEventListener("load", function () {
       $emailErrors = qs("#emailErrors"),
       $password = qs("#password"),
       $passwordErrors = qs("#passwordErrors"),
-      regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
-      regExPass = /^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{6,12}$/;
+      regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
 
       $email.addEventListener('blur', function() {
         switch (true) {
@@ -34,17 +36,48 @@ window.addEventListener("load", function () {
                 $passwordErrors.innerHTML = 'El campo contraseña es obligatorio';
                 $password.classList.add('is-invalid')
                 break;
-            case !regExPass.test($password.value):
-                $passwordErrors.innerHTML = 'Debe ingresar una contraseña válida';
-                $password.classList.add('is-invalid')
-                break
             default:
                 $password.classList.remove('is-invalid');
                 $password.classList.add('is-valid');
                 $passwordErrors.innerHTML = ''
                 break;
         }
-    }
+    },
+    
+    formLogin.addEventListener('submit', event => {
+        event.preventDefault();
+
+        let elementsForm = formLogin.elements;
+        console.log(elementsForm)
+        let error = false;
+
+        for (let i = 0; i < elementsForm.length -2; i++) {
+            
+            if(!elementsForm[i].value){
+                elementsForm[i].classList.add('is-invalid')
+                formError.style.border = "1px solid var(--color-Rojo3)"
+                formError.style.padding = "6px"
+                formError.style.borderRadius = "50px"
+                formError.style.margin = '0 auto'
+                formError.style.marginBottom = '20px'
+                formError.style.color = 'red'
+                formError.innerHTML = "Los campos señalados son obligatorios" + "<i class='fas fa-exclamation-triangle'></i>";
+                error = true
+            }
+        }
+
+        for (let i = 0; i < elementsForm.length - 2; i++) {
+            
+            if(elementsForm[i].classList.contains('is-invalid')){
+                error = true
+            }
+        }
+
+
+        if(error == false){
+            formLogin.submit()
+        }
+    })
 )
 })
 
